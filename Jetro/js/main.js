70d2c593c0b17d17a10a8b58cfab0d451a939c10
reigvenshania <rssdeleon@slsu.edu.ph>
2024-05-14 01:25:30 +0800

    add js query

--- Jetro/js/main.js
+++ Jetro/js/main.js
@@ -1,13 +1,25 @@
 $(document).ready(function() {
+    function toggleAccordion() {
+        var accordionContent = $(this).next('.accordion-content');
+        accordionContent.slideToggle();
+        $('.accordion-content').not(accordionContent).slideUp();
+    }
 
-    $('.accordion-header').on('click'), function() {
-        $(this).next('.accordion-content').slideToggle();
-        $(this).parent('.accordion-item').siblings().find('.accordion-content').slideUp();
+    $('.accordion-header').on('click', toggleAccordion);
 
-        //$(this).find('.fa').toggleClass('fa-minus');
-        //$(this).parent('.accordion-item').siblings().find('.fa-minus').addClass('fa-plus');
+    // Function to handle accordion behavior on small screens
+    function handleAccordionForSmallScreen() {
+        if ($(window).width() <= 767) { // Adjust the breakpoint as needed
+            $('.accordion-header').off('click', toggleAccordion);
+            $('.accordion-header').on('click', toggleAccordion);
+        } else {
+            $('.accordion-header').off('click', toggleAccordion);
+        }
+    }
 
-    });
-  
-  });
-  
+    // Call the function initially
+    handleAccordionForSmallScreen();
+
+    // Call the function whenever the window is resized
+    $(window).on('resize', handleAccordionForSmallScreen);
+});
