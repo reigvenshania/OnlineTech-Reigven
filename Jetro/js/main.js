364a60f8aee215c7c55b0dfd27b943a6f52deda2
reigvenshania <rssdeleon@slsu.edu.ph>
2024-05-14 13:06:13 +0800

    changes in js jetro

--- Jetro/js/main.js
+++ Jetro/js/main.js
@@ -3,12 +3,12 @@ $(document).ready(function(){
     
 
   
-    $('.accordion-header').click(function(){
+    $('.accordion-title').click(function(){
         $(this).toggleClass('active').next('.accordion-content').slideToggle();
 
 
         $('.accordion-content').not($(this).next()).slideUp();
-        $('.accordion-header').not($(this)).removeClass('active');
+        $('.accordion-title').not($(this)).removeClass('active');
     });
 
 
